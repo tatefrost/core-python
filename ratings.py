6ac5ff67d3263a3962dbafce71f3809a13c81079
Tate Frost <tatefrost96@gmail.com>
2022-01-13 11:41:21 -0700

    Fixed main function: now less cluttered

--- ratings.py
+++ ratings.py
@@ -1,7 +1,7 @@
 """Restaurant rating lister."""
 
 
-print("Hello, welcome to your favorite restaurant rater!")
+print("\nHello, welcome to your favorite restaurant rater!")
 
 
 scores = open("scores.txt", "r")
@@ -10,32 +10,32 @@ user_score = 0
 ratings = {}
 
 
+def get_ratings():
+    if user_restaurant != "":
+        ratings[user_restaurant] = user_score
+    for line in scores:
+        pair = line.split(":")
+        ratings[pair[0]] = pair[-1].strip()
+
+def print_ratings_alphabetical():
+    item = ratings.items()
+    alpha = sorted(item)
+    for pairs in alpha:
+        print(str(pairs[0]), "got a rating of", str(pairs[1]) + ".")
+
+def judge_score():
+    global user_restaurant
+    global user_score
+    user_input = input("\nPlease enter a restaurant name: ")
+    user_restaurant = user_input.capitalize()
+    user_score = input("\nPlease enter a rating between 0 and 5 for " + user_restaurant + ": ")
+    if int(user_score) > 5 or int(user_score) < 0:
+        print("\nThat is not a number from 0 to 5, please try again.")
+        judge_score()
+
 def main():
     print("\nWould you like to see all the ratings in alphabetical order?\nPress A\nWould you like to add a new restaurant?\nPress N\nWould you like to Quit?\nPress Q")
     user_operation = input("Enter here: ")
-
-    def judge_score():
-        global user_restaurant
-        global user_score
-        user_input = input("\nPlease enter a restaurant name: ")
-        user_restaurant = user_input.capitalize()
-        user_score = input("\nPlease enter a rating between 0 and 5 for " + user_restaurant + ": ")
-        if int(user_score) > 5 or int(user_score) < 0:
-            print("\nThat is not a number from 0 to 5, please try again.")
-            judge_score()
-
-    def get_ratings():
-        if user_restaurant != "":
-            ratings[user_restaurant] = user_score
-        for line in scores:
-            pair = line.split(":")
-            ratings[pair[0]] = pair[-1].strip()
-
-    def print_ratings_alphabetical():
-        item = ratings.items()
-        alpha = sorted(item)
-        for pairs in alpha:
-            print(str(pairs[0]), "got a rating of", str(pairs[1]) + ".")
 
     if user_operation.lower() == "a":
         get_ratings()
@@ -50,8 +50,8 @@ def main():
         scores.close()
         return
     else:
-      print("\nThat is not a valid command, please try again.")
-      main()
+        print("\nThat is not a valid command, please try again.")
+        main()
 
 
 main()
