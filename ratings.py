9278ae2121e257245087ba09a017672cbd555bf3
Tate Frost <tatefrost96@gmail.com>
2022-01-13 13:23:06 -0700

    Added ability to edit chosen restaurant score

--- ratings.py
+++ ratings.py
@@ -1,5 +1,6 @@
 """Restaurant rating lister."""
 
+from os import EX_TEMPFAIL
 from random import choice
 
 print("\nHello, welcome to your favorite restaurant rater!")
@@ -21,6 +22,7 @@ def get_ratings():
 def print_ratings_alphabetical():
     global item
     alpha = sorted(item)
+    print("\n")
     for pairs in alpha:
         print(str(pairs[0]), "got a rating of", str(pairs[1]) + ".")
 
@@ -35,21 +37,36 @@ def judge_score():
         judge_score()
 
 def update_random():
+    global user_score
     global item
     get_ratings()
     in_list = list(item)
     random_choice = choice(in_list)
     print("\n" + str(random_choice[0]), "got a rating of", str(random_choice[1]) + ".")
     new_rating = input("\nGive " + str(random_choice[0]) + " a new rating: ")
+    user_score = new_rating
     ratings[random_choice[0]] = new_rating
     get_ratings()
     print_ratings_alphabetical()
 
 def update_chosen():
-  return
+  global user_score
+  get_ratings()
+  print_ratings_alphabetical()
+  user_choice = input("\nPlease type the exact name of the restaurant whose score you would like to update: ")
+  new_rating = input("\nPlease enter a new score for " + user_choice + ": ")
+  if user_choice not in ratings:
+      print("\nThat restaurant is not in the list, please try again.\n")
+      update_chosen()
+  else:
+      user_score = new_rating
+      ratings[user_choice] = new_rating
+      get_ratings()
+      print_ratings_alphabetical()
+      return
 
 def main():
-    print("\nWould you like to see all the ratings in alphabetical order?\nPress A\nWould you like to add a new restaurant?\nPress N\nWould you like to update a score on a random restaurant?\nPress UR\nWould you like to Quit?\nPress Q")
+    print("\nWould you like to see all the ratings in alphabetical order?\nPress A\nWould you like to add a new restaurant?\nPress N\nWould you like to update the score on a chosen restaurant?\nPress UC\nWould you like to update a score on a random restaurant?\nPress UR\nWould you like to Quit?\nPress Q")
     user_operation = input("Enter here: \n")
 
     if user_operation.lower() == "a":
@@ -63,6 +80,9 @@ def main():
         main()
     elif user_operation.lower() == "ur":
         update_random()
+        main()
+    elif user_operation.lower() == "uc":
+        update_chosen()
         main()
     elif user_operation.lower() == "q":
         scores.close()
