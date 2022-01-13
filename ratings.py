46a93da9eb60bb6d66e063f8fd17bef08b0bfcf0
Tate Frost <tatefrost96@gmail.com>
2022-01-13 10:17:58 -0700

    Added in score validator

--- ratings.py
+++ ratings.py
@@ -5,9 +5,14 @@ ratings = {}
 
 user_input = input("Please enter a restaurant name: ")
 user_restaurant = user_input.capitalize()
+user_score = 0
 
-user_score = input("Please enter a rating between 0 and 5 for " + user_restaurant + ": ")
-
+def judge_score():
+    global user_score
+    user_score = input("Please enter a rating between 0 and 5 for " + user_restaurant + ": ")
+    if int(user_score) > 5 or int(user_score) < 0:
+        print("That is not a number from 0 to 5, please try again.")
+        judge_score()
 
 def get_ratings():
     ratings[user_restaurant] = user_score
@@ -21,6 +26,7 @@ def print_ratings_alphabetical():
     for pairs in alpha:
         print(pairs[0], "got a rating of", pairs[1] + ".")
 
+judge_score()
 get_ratings()
 print_ratings_alphabetical()
 
